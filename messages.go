1b322107fc346ca593991f7961eecd9deb82bd51
Ramey Girdhar <ramey@plivo.com>
2018-08-29 12:14:18 +0530

    adding comment

--- messages.go
+++ messages.go
@@ -9,9 +9,10 @@ type MessageCreateParams struct {
 	Dst  string `json:"dst,omitempty" url:"dst,omitempty"`
 	Text string `json:"text,omitempty" url:"text,omitempty"`
 	// Optional parameters.
-	Type          string `json:"type,omitempty" url:"type,omitempty"`
-	URL           string `json:"url,omitempty" url:"url,omitempty"`
-	Method        string `json:"method,omitempty" url:"method,omitempty"`
+	Type   string `json:"type,omitempty" url:"type,omitempty"`
+	URL    string `json:"url,omitempty" url:"url,omitempty"`
+	Method string `json:"method,omitempty" url:"method,omitempty"`
+	// Either one of src and powerpackuuid should be given
 	PowerpackUUID string `json:"powerpack_uuid,omitempty" url:"powerpack_uuid,omitempty"`
 }
 
