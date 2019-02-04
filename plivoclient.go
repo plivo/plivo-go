44584bf9e33aeec6c9bd103bd85249145ec32a2e
Suhas Reddy Mulkalla <suhasreddy.1993@gmail.com>
2019-02-04 10:29:09 +0530

    [SDKS-157] Address verfication and Identity verification api's in Go

--- plivoclient.go
+++ plivoclient.go
@@ -30,6 +30,9 @@ type Client struct {
 	LiveCalls    *LiveCallService
 	QueuedCalls  *QueuedCallService
 	Conferences  *ConferenceService
+	Addresses    *AddressService
+	Identities   *IdentityService
+
 }
 
 /*
@@ -85,6 +88,8 @@ func NewClient(authId, authToken string, options *ClientOptions) (client *Client
 	client.LiveCalls = &LiveCallService{client: client}
 	client.QueuedCalls = &QueuedCallService{client: client}
 	client.Conferences = &ConferenceService{client: client}
+	client.Addresses = &AddressService{client:client}
+	client.Identities = &IdentityService{client:client}
 
 	return
 }
