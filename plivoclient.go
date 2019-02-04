9d1472827258c975e063f97f5dccbe56350c5849
Suhas Reddy Mulkalla <suhasreddy.1993@gmail.com>
2019-02-04 11:56:05 +0530

    [SDKS-157] Address verfication and Identity verification

--- plivoclient.go
+++ plivoclient.go
@@ -42,6 +42,8 @@ type Client struct {
 	LiveCalls    *LiveCallService
 	QueuedCalls  *QueuedCallService
 	Conferences  *ConferenceService
+	Addresses    *AddressService
+	Identities   *IdentityService
 
 	RequestInterceptor  func(request *http.Request)
 	ResponseInterceptor func(response *http.Response)
@@ -108,6 +110,8 @@ func NewClient(authId, authToken string, options *ClientOptions) (client *Client
 	client.LiveCalls = &LiveCallService{client: client}
 	client.QueuedCalls = &QueuedCallService{client: client}
 	client.Conferences = &ConferenceService{client: client}
+	client.Addresses = &AddressService{client:client}
+	client.Identities = &IdentityService{client:client}
 
 	return
 }
