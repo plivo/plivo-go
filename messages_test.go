9510164e2ed44e669ee6fae2abeab34dca97fc4d
narayana shanubhogh <narayana@plivo.com>
2019-10-15 13:34:00 +0530

    mms changes

--- messages_test.go
+++ messages_test.go
@@ -38,6 +38,47 @@ func TestMessageService_Get(t *testing.T) {
 	assertRequest(t, "GET", "Message/%s", uuid)
 }
 
+func TestMessageService_GetMedia(t *testing.T) {
+	expectResponse("mmsMediaGetResponse.json", 200)
+	uuid := "f734eeec-e59f-11e9-89dc-0242ac110003"
+	mediaID := "91f8f9ce-e236-474f-a2b8-f75d9c35988a"
+
+	if _, err := client.Messages.GetMedia(GetMediaParams{
+		MessageUUID: uuid,
+		MediaID:     mediaID,
+	}); err != nil {
+		panic(err)
+	}
+	cl := client.httpClient
+	client.httpClient = nil
+	if _, err := client.Messages.GetMedia(GetMediaParams{
+		MessageUUID: uuid,
+		MediaID:     mediaID,
+	}); err == nil {
+		panic(errors.New("error expected"))
+	}
+	client.httpClient = cl
+
+	assertRequest(t, "GET", "Message/%s/Media/%s", uuid, mediaID)
+}
+
+func TestMessageService_ListMedia(t *testing.T) {
+	expectResponse("mmsMediaListResponse.json", 200)
+	uuid := "5b40a428-bfc7-4daf-9d06-726c558bf3b8"
+
+	if _, err := client.Messages.ListMedia(uuid); err != nil {
+		panic(err)
+	}
+	cl := client.httpClient
+	client.httpClient = nil
+	if _, err := client.Messages.ListMedia(uuid); err == nil {
+		panic(errors.New("error expected"))
+	}
+	client.httpClient = cl
+
+	assertRequest(t, "GET", "Message/%s/Media", uuid)
+}
+
 func TestMessageService_Create(t *testing.T) {
 	expectResponse("messageSendResponse.json", 202)
 
