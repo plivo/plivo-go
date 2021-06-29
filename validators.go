bf229b0e1f9d474491a402af0e3e863a2b168bb5
LSAITHARUN <saitharunreddylokireddy@gmail.com>
2021-06-29 16:05:44 +0530

    Added Mpc enhancements like Callername,Parallel Behaviour for DelayDial and RingTimeout and Participant level recording.

--- validators.go
+++ validators.go
@@ -2,21 +2,33 @@ package plivo
 
 import (
 	"github.com/sirupsen/logrus"
+	"reflect"
 	"strconv"
 	"strings"
 )
 
-func MultipleValidIntegers(paramname string,paramvalue string, lowerbound int,upperbound int){
-	values := strings.SplitN(paramvalue,"<",-1)
-	for i :=0;i<len(values);i++{
-		val, err := strconv.Atoi(values[i])
-		if err!=nil{
-			logrus.Fatal(paramname + " values passed in the string must be integers")
-		}else{
-			if val < lowerbound || val >upperbound{
-				error := paramname + " values passed must be in the range [" + strconv.Itoa(lowerbound) + " , " + strconv.Itoa(upperbound) + "]"
-				logrus.Fatal(error)
+func MultipleValidIntegers(paramname string, paramvalue interface{}, lowerbound int, upperbound int) {
+	if reflect.TypeOf(paramvalue).Kind() == reflect.Int {
+		paramvalue := paramvalue.(int)
+		if paramvalue < lowerbound || paramvalue > upperbound {
+			error := paramname + " values must be in the range [" + strconv.Itoa(lowerbound) + " , " + strconv.Itoa(upperbound) + "]"
+			logrus.Fatal(error)
+		}
+	} else if reflect.TypeOf(paramvalue).Kind() == reflect.String {
+		paramvalue := paramvalue.(string)
+		values := strings.SplitN(paramvalue, "<", -1)
+		for i := 0; i < len(values); i++ {
+			val, err := strconv.Atoi(values[i])
+			if err != nil {
+				logrus.Fatal(paramname + " Destination values in the string must be integers")
+			} else {
+				if val < lowerbound || val > upperbound {
+					error := paramname + " Destination values must be in the range [" + strconv.Itoa(lowerbound) + " , " + strconv.Itoa(upperbound) + "]"
+					logrus.Fatal(error)
+				}
 			}
 		}
+	} else {
+		logrus.Fatal(paramname + " must be either string or integer")
 	}
 }
