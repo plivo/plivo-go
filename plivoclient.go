87eaabde5e5914e117f319af8dcea375d0bba2d1
Koushik-Ayila <koushik.ayila@plivo.com>
2021-12-10 11:46:48 +0530

    Route Voice traffic via AKAMAI

--- plivoclient.go
+++ plivoclient.go
@@ -12,9 +12,9 @@ import (
 const baseUrlString = "https://api.plivo.com/"
 
 var HttpsScheme = "https"
-var voiceBaseUrlString = "voice.plivo.com"
-var voiceBaseUrlStringFallback1 = "voice-usw1.plivo.com"
-var voiceBaseUrlStringFallback2 = "voice-use1.plivo.com"
+var voiceBaseUrlString = "api.plivo.com"
+var voiceBaseUrlStringFallback1 = "api.plivo.com"
+var voiceBaseUrlStringFallback2 = "api.plivo.com"
 
 const baseRequestString = "/v1/Account/%s/"
 
