31ddad7854af50c78261c2329bb8c4a760a4c78b
kowshiksiva5 <kowshik.siva@students.iiit.ac.in>
2022-08-05 09:55:10 +0530

    Change in token creation format

--- token.go
+++ token.go
@@ -1,7 +1,5 @@
 package plivo
 
-import "errors"
-
 type TokenService struct {
 	client *Client
 }
@@ -10,35 +8,37 @@ type TokenCreateParams struct {
 	// Required parameters.
 	Iss string `json:"iss,omitempty" url:"iss,omitempty"`
 	// Optional parameters.
-	Sub            string `json:"sub,omitempty" url:"sub,omitempty"`
-	Exp            int64  `json:"exp,omitempty" url:"exp,omitempty"`
-	Nbf            int64  `json:"nbf,omitempty" url:"nbf,omitempty"`
-	Incoming_allow bool   `json:"incoming_allow,omitempty" url:"incoming_allow,omitempty"`
-	Outgoing_allow bool   `json:"outgoing_allow,omitempty" url:"outgoing_allow,omitempty"`
-	App            string `json:"app,omitempty" url:"app,omitempty"`
+	Sub           string      `json:"Sub,omitempty" url:"Sub,omitempty"`
+	Exp           int64       `json:"Exp,omitempty" url:"Exp,omitempty"`
+	Nbf           int64       `json:"Nbf,omitempty" url:"Nbf,omitempty"`
+	IncomingAllow interface{} `json:"incoming_allow,omitempty" url:"incoming_allow,omitempty"`
+	OutgoingAllow interface{} `json:"outgoing_allow,omitempty" url:"outgoing_allow,omitempty"`
+	Per           interface{} `json:"per,omitempty" url:"per,omitempty"`
+	App           string      `json:"App,omitempty" url:"App,omitempty"`
 }
 
 // Stores response for creating a token.
+
 type TokenCreateResponse struct {
 	Token string `json:"token" url:"token"`
 	ApiID string `json:"api_id" url:"api_id"`
 }
 
 func (service *TokenService) Create(params TokenCreateParams) (response *TokenCreateResponse, err error) {
-	per := make(map[string]interface{})
-	per["voice"] = make(map[string]interface{})
+	temp := map[string]interface{}{}
+	if params.IncomingAllow != nil || params.OutgoingAllow != nil {
 
-	if params.Incoming_allow {
-		per["voice"].(map[string]interface{})["incoming_allow"] = params.Incoming_allow
+		temp["per"] = map[string]interface{}{}
+		temp["per"].(map[string]interface{})["voice"] = map[string]interface{}{}
+		if params.IncomingAllow != nil {
+			temp["per"].(map[string]interface{})["voice"].(map[string]interface{})["incoming_allow"] = params.IncomingAllow
+		}
+		if params.OutgoingAllow != nil {
+			temp["per"].(map[string]interface{})["voice"].(map[string]interface{})["outgoing_allow"] = params.OutgoingAllow
+		}
+		params.Per = temp
 	}
 
-	if params.Outgoing_allow {
-		per["voice"].(map[string]interface{})["outgoing_allow"] = params.Outgoing_allow
-	}
-
-	if params.Incoming_allow && params.Sub == "" {
-		return nil, errors.New("sub is required when incoming_allow is true")
-	}
 	req, err := service.client.NewRequest("POST", params, "JWT/Token")
 	if err != nil {
 		return
