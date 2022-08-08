5bc9a7d660bbf2e903a7b7686edc7deb104918d7
kowshiksiva5 <kowshik.siva@students.iiit.ac.in>
2022-08-08 13:52:56 +0530

    Edited token.go file to accept sub

--- token.go
+++ token.go
@@ -8,13 +8,13 @@ type TokenCreateParams struct {
 	// Required parameters.
 	Iss string `json:"iss,omitempty" url:"iss,omitempty"`
 	// Optional parameters.
-	Sub           string      `json:"Sub,omitempty" url:"Sub,omitempty"`
-	Exp           int64       `json:"Exp,omitempty" url:"Exp,omitempty"`
-	Nbf           int64       `json:"Nbf,omitempty" url:"Nbf,omitempty"`
+	Exp           int64       `json:"exp,omitempty" url:"Exp,omitempty"`
+	Nbf           int64       `json:"nbf,omitempty" url:"Nbf,omitempty"`
 	IncomingAllow interface{} `json:"incoming_allow,omitempty" url:"incoming_allow,omitempty"`
 	OutgoingAllow interface{} `json:"outgoing_allow,omitempty" url:"outgoing_allow,omitempty"`
 	Per           interface{} `json:"per,omitempty" url:"per,omitempty"`
-	App           string      `json:"App,omitempty" url:"App,omitempty"`
+	App           string      `json:"app,omitempty" url:"App,omitempty"`
+	Sub           string      `json:"sub,omitempty" url:"Sub,omitempty"`
 }
 
 // Stores response for creating a token.
@@ -39,6 +39,7 @@ func (service *TokenService) Create(params TokenCreateParams) (response *TokenCr
 			params.Per = permissionsMap
 		}
 	}
+
 	req, err := service.client.NewRequest("POST", params, "JWT/Token")
 	if err != nil {
 		return
