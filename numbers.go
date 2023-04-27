3974aed471af58c330641b6ba577dfb94d9272ba
prabhav-plivo <prabhav.bansal@plivo.com>
2023-04-27 11:04:04 +0530

    add renewal date in numbers struct

--- numbers.go
+++ numbers.go
@@ -18,6 +18,7 @@ type Number struct {
 	NumberType               string `json:"number_type,omitempty" url:"number_type,omitempty"`
 	MonthlyRentalRate        string `json:"monthly_rental_rate,omitempty" url:"monthly_rental_rate,omitempty"`
 	Application              string `json:"application,omitempty" url:"application,omitempty"`
+	RenewalDate              string `json:"renewal_date,omitempty" url:"renewal_date,omitempty"`
 	AddedOn                  string `json:"added_on,omitempty" url:"added_on,omitempty"`
 	ResourceURI              string `json:"resource_uri,omitempty" url:"resource_uri,omitempty"`
 	VoiceRate                string `json:"voice_rate,omitempty" url:"voice_rate,omitempty"`
