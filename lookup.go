e4361510f04c7b002c79b24e6a66b2119259bba8
Prashant Pandey <prashant.pandey@plivo.com>
2024-09-04 02:02:42 +0530

    VT-8038: Fix lint issues

--- lookup.go
+++ lookup.go
@@ -9,6 +9,11 @@ import (
 // this imposes the limitation that each struct type has to be unique across
 // all of Plivo's product APIs.
 
+const (
+	numberString = "v1/Number/%s"
+	format       = "%s"
+)
+
 type Country struct {
 	Name string `json:"name"`
 	ISO2 string `json:"iso2"`
@@ -56,7 +61,7 @@ func (s *LookupService) Get(number string, params LookupParams) (*LookupResponse
 		params.Type = "carrier"
 	}
 
-	req, err := s.client.BaseClient.NewRequest("GET", params, fmt.Sprintf("v1/Number/%s", "%s"), number)
+	req, err := s.client.BaseClient.NewRequest("GET", params, fmt.Sprintf(numberString, format), number)
 	if err != nil {
 		return nil, err
 	}
