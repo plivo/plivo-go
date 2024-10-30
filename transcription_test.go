c41bf4e852e3cae1462f7a4ffc57f2c680df2a34
ajay-plivo <ajaykumar.verma@plivo.com>
2024-10-30 16:27:51 +0530

    VT-8376:unitTestCasesFuxed

--- transcription_test.go
+++ transcription_test.go
@@ -7,7 +7,7 @@ import (
 
 // without queryParam passed
 func TestTranscriptionService_GetRecordingTranscription(t *testing.T) {
-	expectResponse("GetRecordingTranscription.json", 200)
+	expectResponse("getRecordingTranscription.json", 200)
 
 	if _, err := client.Transcription.GetRecordingTranscription(GetRecordingTranscriptionRequest{
 		TranscriptionID: "e12d05fe-6979-485c-83dc-9276114dba3b",
@@ -27,7 +27,7 @@ func TestTranscriptionService_GetRecordingTranscription(t *testing.T) {
 
 // with queryParam
 func TestTranscriptionService_GetRecordingTranscriptionWithParam(t *testing.T) {
-	expectResponse("GetRecordingTranscription.json", 200)
+	expectResponse("getRecordingTranscription.json", 200)
 
 	if _, err := client.Transcription.GetRecordingTranscription(GetRecordingTranscriptionRequest{
 		TranscriptionID:   "e12d05fe-6979-485c-83dc-9276114dba3b",
